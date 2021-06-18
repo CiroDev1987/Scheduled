fd3823e28afaa839404fbbe359fd267b03ea7e45
CiroDev1987 <cirodev.1987@gmail.com>
2021-06-18 16:58:28 -0300

    primeiro commit

---
+++ src/main/java/com/example/emailscheduler1/EmailScheduler1Application.java
@@ -0,0 +1,13 @@
+package com.example.emailscheduler1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailScheduler1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailScheduler1Application.class, args);
+	}
+
+}
