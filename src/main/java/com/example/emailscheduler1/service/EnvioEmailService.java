f75c0a476d087a7f62b0c761bc6fce1bed86c4d9
CiroDev1987 <cirodev.1987@gmail.com>
2021-06-28 14:09:09 -0300

    Scheduled Email service 2

--- src/main/java/com/example/emailscheduler1/service/EnvioEmailService.java
+++ src/main/java/com/example/emailscheduler1/service/EnvioEmailService.java
@@ -1,12 +1,20 @@
 package com.example.emailscheduler1.service;
 
 import lombok.RequiredArgsConstructor;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 @RequiredArgsConstructor
 public class EnvioEmailService {
 
-    static final String
+    private Object EmailUtil;
+
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        System.out.println("tarde" + new EnvioEmailService());
+    }
 
 }
